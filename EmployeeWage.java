a3e34bb4a709e5bf8e5587a7f5a8d4b023c77ad5
pratikshad16 <dhagepratiksha16@gmail.com>
2020-07-18 10:48:22 +0530

    Added: Attendance functionality for employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,7 +2,13 @@
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-			System.out.println("Welcome to Employee wage program");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10 ) %2;
+		System.out.println("Welcome to Employee Wage Program");
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present.");
+		else
+			System.out.println("Employee is Absent.");
 		
 
 	}
