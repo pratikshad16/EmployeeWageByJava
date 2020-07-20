41f8259a681c4e4a94ff3841e1e94cb22938f179
pratikshad16 <dhagepratiksha16@gmail.com>
2020-07-20 23:40:42 +0530

    Added: functionality for wages for multiple comapnies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,21 +1,37 @@
 
 public class EmployeeWage {
 
+		public static final int IS_ABSENT = 0;
 		public static final int IS_PART_TIME = 1;
 		public static final int IS_FULL_TIME = 2;
-		public static final int WAGE_PER_HOUR = 20;
 		
-		public static int computeEmployeeWage() {
-		int dailyWage=0;
-		int empHrs=0;
-		int totalWorkingHrs = 0;
-		int totalWorkingDays = 0;
+		private static String company;
+		private static int workingHr;
+		private static  int WAGE_PER_HOUR;
+		private static int MAX_HR_IN_MONTH;
+		private static int NUM_OF_WORKING_DAYS;
+		
+		  @SuppressWarnings("static-access")
+		EmployeeWage(String company,int WAGE_PER_HOUR,int workingHr,int MAX_HR_IN_MONTH,int NUM_OF_WORKING_DAYS)
+		 {
+			  this.company=company;
+			  this.WAGE_PER_HOUR=WAGE_PER_HOUR;
+			  this.workingHr=workingHr;
+			  this.MAX_HR_IN_MONTH=MAX_HR_IN_MONTH;
+			  this.NUM_OF_WORKING_DAYS=NUM_OF_WORKING_DAYS;
+		 }
+		  public static void monthlyWage()
+		  {
+			 int empHrs=0;
+			 int empWage=0;
+			 int totalWorkingHrs = 0;
+			 int totalWorkingDays = 0;
+			 int totalWage=0;
 		
 		//computation
-		System.out.println("Welcome to Employee Wage Program");
-		while(totalWorkingHrs <= 100 && totalWorkingDays <= 20)
+		while(totalWorkingHrs <= MAX_HR_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
 		{
-			int totalWage=0;
+		
 			totalWorkingDays++;
 			int empCheck = (int)Math.floor(Math.random() * 10)%3;
 			switch (empCheck)
@@ -32,15 +48,21 @@ public class EmployeeWage {
 			}
 			
 			totalWorkingHrs+=empHrs;
-			System.out.println("Days:"+totalWorkingDays+   " Employees hrs: "+empHrs);
+			empWage=empHrs*WAGE_PER_HOUR;
+			totalWage+=empWage;
 		}
-		int totalWage=totalWorkingHrs*WAGE_PER_HOUR;
-		System.out.println("Total wage of employee:"+totalWage);
-		return totalWage;
+		System.out.println("Monthly wage of employee of "+company+" is:"+totalWage);
 		
 	}
+		@SuppressWarnings("static-access")
 		public static void main(String[] args) {
-			computeEmployeeWage();
+			EmployeeWage company1 = new EmployeeWage("DMart", 20, 7, 100, 25);
+			company1.monthlyWage();
+			EmployeeWage company2 = new EmployeeWage("Reliance", 15, 8, 120, 30);
+			company2.monthlyWage();
+			EmployeeWage company3 = new EmployeeWage("Big basket", 25, 9, 130, 35);
+			company3.monthlyWage();
+		
 		}
 }
 
