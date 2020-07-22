9229c4972e5bdf6c5b0c4f7a589a0bc819742091
pratikshad16 <dhagepratiksha16@gmail.com>
2020-07-22 20:37:44 +0530

    Added: Functinlaity to get a total wage when querified by comapny

--- Employee/EmployeeWage.java
+++ Employee/EmployeeWage.java
@@ -16,9 +16,9 @@ class computeEmployeeWage {
 		
 		  @SuppressWarnings("static-access")
 		  public static HashMap<Integer, Integer> companyWage = new HashMap<Integer, Integer>();
-	      public static ArrayList<Integer> wagePerCompany = new ArrayList <Integer>();
-		  public static ArrayList<Integer> Company1 = new ArrayList <Integer>();
-	      //public static ArrayList<Integer> Company2 = new ArrayList <Integer>();
+	      public static ArrayList<Integer> CompanyOne = new ArrayList <Integer>();
+	      public static ArrayList<Integer> CompanyTwo = new ArrayList <Integer>();
+		  public static HashMap<String, Integer> companyMonthlyWage = new HashMap<>();
 		@SuppressWarnings("static-access")
 		computeEmployeeWage(String company,int WAGE_PER_HOUR,int MAX_HR_IN_MONTH,int NUM_OF_WORKING_DAYS)
 		 {
@@ -27,10 +27,11 @@ class computeEmployeeWage {
 			  this.MAX_HR_IN_MONTH=MAX_HR_IN_MONTH;
 			  this.NUM_OF_WORKING_DAYS=NUM_OF_WORKING_DAYS;
 		 }
-		  public String getCompany()
-		  {
-			  return this.company;
-		  }
+		public String getCompany() 
+		{
+			return this.company;
+		}
+		  
 		  public static int employeeDailyWage() 
 			{
 				empWage = fullDayHr * WAGE_PER_HOUR;
@@ -65,17 +66,20 @@ class computeEmployeeWage {
 			
 			totalWorkingHrs+=empHrs;
 			empWage=empHrs*WAGE_PER_HOUR;
+			if(company=="DMart")
+				CompanyOne.add(empWage);
+			else
+				CompanyTwo.add(empWage);
 			totalWage+=empWage;
 		}
 		//System.out.println("Monthly wage of employee of "+company+" is:"+totalWage);
 		return totalWage;
 		
 	}
-		  public static void dailyWageAndMonthlyWage() 
-			{
-				companyWage.put(employeeDailyWage(), monthlyWage());
-				entry++;
-			}
+		 // public static void dailyWageAndMonthlyWage() 
+			//{
+				//companyWage.put(employeeDailyWage(), monthlyWage());
+			//}
 
 			public static void printWage() 
 			{
@@ -83,25 +87,40 @@ class computeEmployeeWage {
 					System.out.println("Daily Wage:"+i+" Total Wage:"+companyWage.get(i));
 			}
 
-			public static void companyWages() 
-			{
-				wagePerCompany.add(monthlyWage());
-			}
+			//public static void companyWages() 
+			//{
+				//wagePerCompany.add(monthlyWage());
+			//}
 
 			public static void printCompanyWage() 
 			{
-				System.out.println(wagePerCompany);
+				System.out.println(CompanyOne);
+				System.out.println(CompanyTwo);
+			}
+			public static void putTotalWage() 
+			{
+				int a=monthlyWage();
+				companyMonthlyWage.put(company, a);
+				companyWage.put(employeeDailyWage(), a);
+				entry++;
+			}
+
+			public static void getTotalWage(String company) 
+			{
+				System.out.println(company+" : "+companyMonthlyWage.get(company));
 			}
 }
 
 public class EmployeeWage {
 	
 	public static void main(String[] args) {
-		computeEmployeeWage company1 = new computeEmployeeWage("DMart", 20, 100, 25);
-		company1.dailyWageAndMonthlyWage();
-		computeEmployeeWage company2 = new computeEmployeeWage("Reliance", 15, 120, 30);
-		company2.dailyWageAndMonthlyWage();
-		company2.printWage();
+		computeEmployeeWage companyOne = new computeEmployeeWage("DMart", 20, 100, 25);
+		companyOne.putTotalWage();
+		computeEmployeeWage companyTwo = new computeEmployeeWage("Reliance", 15, 120, 30);
+		companyTwo.putTotalWage();
+		companyTwo.printWage();
+		companyTwo.printCompanyWage();
+		companyTwo.getTotalWage("Reliance");
 	}
 
 }
